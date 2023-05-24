9587f7cc06c6169b5b3f4588a57a085c2a030888
Denis Litinskiy <plnvoran@mail.ru>
2023-05-24 21:36:57 +0300

    Убрал пробелы

--- src/test/java/com/demoqa/pages/CbrHomePage.java
+++ src/test/java/com/demoqa/pages/CbrHomePage.java
@@ -8,63 +8,48 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class CbrHomePage {
 
-    // Locators
     SelenideElement topHorizontalMenu = $(".home-header_top"),
             headerLogoRu = $(".home-header_top .header_logo"),
-            headerLogoEn= $(".home-header_top .header_logo_eng"),
+            headerLogoEn = $(".home-header_top .header_logo_eng"),
             headerLeftMenu = $(".home-header_top .header_menu"),
             activeSlide = $(".home-header_main .swiper-slide-active"),
-            languageActive= $(".home-header_top .header_lang>._active"),
+            languageActive = $(".home-header_top .header_lang>._active"),
             languageNoActive = $(".home-header_top .header_lang_item>a");
 
-
-    // Actions
     public CbrHomePage openPage() {
         open("/");
         return this;
     }
 
-
-    public CbrHomePage checkHomeTitle(String  language) {
-
-        if (language.equals("RU"))
-        {
+    public CbrHomePage checkHomeTitle(String language) {
+        if (language.equals("RU")) {
             assertEquals("Центральный банк Российской Федерации | Банк России", Selenide.title());
-        }
-        else
+        } else
             assertEquals("The Central Bank of Russian Federation | Bank of Russia", Selenide.title());
-
         return this;
-
     }
 
     public CbrHomePage checkTopHorizontalMenuIsVisible() {
-
         topHorizontalMenu.should(visible);
         return this;
     }
 
     public CbrHomePage checkHeaderLogoIsVisible(String language) {
-
-        if (language.equals("RU"))
-        {
+        if (language.equals("RU")) {
             headerLogoRu.should(visible);
-        }
-        else
+        } else
             headerLogoEn.should(visible);
-
         return this;
     }
 
-
-    public CbrHomePage  checkHeaderLeftMenuIsVisible() {
+    public CbrHomePage checkHeaderLeftMenuIsVisible() {
         headerLeftMenu.should(visible);
         return this;
     }
-    public CbrHomePage  checkActiveSlideIsVisible() {
+
+    public CbrHomePage checkActiveSlideIsVisible() {
         activeSlide.should(visible);
         return this;
     }
@@ -79,9 +64,8 @@ public class CbrHomePage {
         return this;
     }
 
-    public CbrHomePage  checkNoActiveLanguage(String language) {
+    public CbrHomePage checkNoActiveLanguage(String language) {
         languageNoActive.shouldHave(text(language));
         return this;
     }
-
 }
--- src/test/java/com/demoqa/tests/CbrHomeWithRemoteTests.java
+++ src/test/java/com/demoqa/tests/CbrHomeWithRemoteTests.java
@@ -7,83 +7,75 @@ import org.junit.jupiter.api.Test;
 import static io.qameta.allure.Allure.step;
 
 @Tag("practice_form")
-public class CbrHomeWithRemoteTests extends RemoteTestBase{
+public class CbrHomeWithRemoteTests extends RemoteTestBase {
 
     @Test
-    @Tag("smoke") @Tag("blocker")
+    @Tag("smoke")
+    @Tag("blocker")
     @DisplayName("Check main elements on RU Home")
     void checkMainElementsOnHomeRu() {
         step("Open home", () -> {
             cbrHomePage.openPage();
         });
-
-
         step("Check main elements on Home", () -> {
             cbrHomePage
                     .checkTopHorizontalMenuIsVisible()
                     .checkHeaderLeftMenuIsVisible()
                     .checkHeaderLogoIsVisible("RU")
                     .checkActiveSlideIsVisible();
-
         });
-
     }
+
     @Test
-    @Tag("smoke") @Tag("blocker")
+    @Tag("smoke")
+    @Tag("blocker")
     @DisplayName("Check that default language is RU")
     void checkDefaultLanguage() {
         step("Open home", () -> {
             cbrHomePage.openPage();
         });
-
         step("Check main elements on home page", () -> {
             cbrHomePage.checkActiveLanguage("RU");
-
         });
-
         step("Check RU Home title", () -> {
             cbrHomePage.checkHomeTitle("RU");
 
         });
-
     }
 
     @Test
-    @Tag("smoke") @Tag("blocker")
+    @Tag("smoke")
+    @Tag("blocker")
     @DisplayName("Check available languages")
     void checkAvailableLanguages() {
         step("Open home", () -> {
             cbrHomePage.openPage();
         });
-
         step("Check available languages", () -> {
             cbrHomePage.checkNoActiveLanguage("EN");
             cbrHomePage.checkActiveLanguage("RU");
         });
-
     }
 
     @Test
-    @Tag("smoke") @Tag("blocker")
+    @Tag("smoke")
+    @Tag("blocker")
     @DisplayName("Check swithing to English")
     void switchToEnglish() {
         step("Open home", () -> {
             cbrHomePage.openPage();
         });
-
         step("Switch to English", () -> {
             cbrHomePage.switchToLanguage("EN");
-
         });
-
         step("Check EN Home title", () -> {
             cbrHomePage.checkHomeTitle("EN");
-
         });
     }
 
     @Test
-    @Tag("smoke") @Tag("blocker")
+    @Tag("smoke")
+    @Tag("blocker")
     @DisplayName("Check main elements on EN Home")
     void checkMainElementsOnHomeEn() {
         step("Open home", () -> {
@@ -92,7 +84,6 @@ public class CbrHomeWithRemoteTests extends RemoteTestBase{
 
         step("Switch to English", () -> {
             cbrHomePage.switchToLanguage("EN");
-
         });
 
         step("Check main elements on Home", () -> {
@@ -101,11 +92,9 @@ public class CbrHomeWithRemoteTests extends RemoteTestBase{
                     .checkHeaderLeftMenuIsVisible()
                     .checkHeaderLogoIsVisible("EN")
                     .checkActiveSlideIsVisible();
-
         });
     }
-
-    }
+}
 
 
 
